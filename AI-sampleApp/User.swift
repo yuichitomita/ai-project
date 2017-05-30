9010d3bd4384e30a51d53dcb57c475c6232421be
yuichitomita <y.tomita@speedlink-td.info>
2017-05-30 22:52:27 +0900

    ユーザーデータモデル作成

---
+++ AI-sampleApp/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/05/30.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    // ユーザーのid
+    let id: String
+    
+    // 会社名
+    let companyName: String
+    
+    // プロフィール画像URL
+    let profileImageURL: String
+}
