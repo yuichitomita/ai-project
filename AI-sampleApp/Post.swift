d267b8deb5b002e7c668a55f87f0f6514bb19d49
yuichitomita <y.tomita@speedlink-td.info>
2017-05-30 22:57:14 +0900

    投稿データモデル作成

---
+++ AI-sampleApp/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/05/30.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import Foundation
+struct Post {
+    let id: String
+    
+    //本文
+    let text: String
+    
+    let user: User
+    
+}
