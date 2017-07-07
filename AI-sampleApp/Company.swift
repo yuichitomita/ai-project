c85b980ffc7e460974ebd7c148c3add45ba6bc54
yuichitomita <y.tomita@speedlink-td.info>
2017-07-07 21:35:57 +0900

    ファイル名変更

---
+++ AI-sampleApp/Company.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/05/30.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import Foundation
+
+struct Company {
+    // ユーザーのid
+    let id: String
+    
+    // 会社名
+    let companyName: String
+    
+    //業種
+    let companyType: String
+    
+    // プロフィール画像URL
+    let profileImageURL: String
+}
