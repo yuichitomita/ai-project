3fd42be1c78c258998f8cc67b796606f7551e85e
yuichitomita <y.tomita@speedlink-td.info>
2017-07-07 22:15:06 +0900

    dataSourceを設定

--- AI-sampleApp/MessageListViewController.swift
+++ AI-sampleApp/MessageListViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class MessageListViewController: UIViewController {
-
     @IBOutlet weak var tableView: UITableView!
+    
+    // テーブル表示用のデータソース
+    var posts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.delegate = self
+        tableView.dataSource = self
         
     }
 }
@@ -23,5 +26,30 @@ extension MessageListViewController: UITableViewDelegate {
     //cellがタップされたのを検知した時に実行する処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("セルがタップされたよ！")
+    }
+    
+    // セルの見積もりの高さを指定する処理
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    // セルの高さを指定する処理
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // UITableViewCellの高さを自動で取得する値
+        return UITableViewAutomaticDimension
+    }
+}
+
+extension MessageListViewController: UITableViewDataSource {
+    // 何個のcellを生成するかを設定する関数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    // 描画するcellを設定する関数
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostsTableViewCell") as! PostsTableViewCell
+        
+        return cell
     }
 }
