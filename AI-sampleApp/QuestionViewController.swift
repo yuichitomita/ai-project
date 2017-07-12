94d1bb3ac92972845e9e187a6c47fe4edae1e448
yuichitomita <y.tomita@speedlink-td.info>
2017-07-12 23:10:09 +0900

    質問用コントローラー追加

---
+++ AI-sampleApp/QuestionViewController.swift
@@ -0,0 +1,61 @@
+//
+//  QuestionViewController.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/07/12.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+}
+
+
+extension QuestionViewController: UITableViewDelegate {
+    //cellがタップされたのを検知した時に実行する処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("セルがタップされたよ！")
+    }
+    
+    // セルの見積もりの高さを指定する処理
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    // セルの高さを指定する処理
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // UITableViewCellの高さを自動で取得する値
+        return UITableViewAutomaticDimension
+    }
+}
+
+extension QuestionViewController: UITableViewDataSource {
+    // 何個のcellを生成するかを設定する関数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // 描画するcellを設定する関数
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell")
+        return cell!
+    }
+}
+
+
