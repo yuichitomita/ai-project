15fcc6313680de84ad115c18a836bd5348a3f2c3
yuichitomita <y.tomita@speedlink-td.info>
2017-07-15 03:36:14 +0900

    質問のデータモデル作成

---
+++ AI-sampleApp/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/07/15.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let id: String
+    let text: String
+}
