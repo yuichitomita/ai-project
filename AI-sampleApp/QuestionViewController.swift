1a25d3417dce11d4dfd085de9e0313b407c10fa9
yuichitomita <y.tomita@speedlink-td.info>
2017-07-15 04:12:45 +0900

    ダミーデータ作成

--- AI-sampleApp/QuestionViewController.swift
+++ AI-sampleApp/QuestionViewController.swift
@@ -11,18 +11,56 @@ import UIKit
 class QuestionViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    // テーブル表示用のデータソース
+    var questions: [Question] = [Question]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setup()
         tableView.delegate = self
         tableView.dataSource = self
-
+        
+        // ダミーデータ作成
+        let q1  = Question(id: "01", text: "何かを考え、無口になることが多い。")
+        let q2  = Question(id: "02", text: "活発な方で、体を動かすことは苦にならない。")
+        let q3  = Question(id: "03", text: "いったん決心したことは、ほとんど変えることはない。")
+        let q4  = Question(id: "04", text: "たとえ遊びでも、自分の得意でないことは、最初からやらないことにしている。")
+        let q5  = Question(id: "05", text: "物事を終えたあと、手落ちがなかったか何度も見直したり確認をする。")
+        let q6  = Question(id: "06", text: "掃除などをする時は、徹底的にしないと気がすまない。")
+        let q7  = Question(id: "07", text: "ちょっとしたことでも、人と違ったやり方を考える。")
+        let q8  = Question(id: "08", text: "いろいろと動きまわるより、静かに落ち着いている方が好きである。")
+        let q9  = Question(id: "09", text: "さきの先まで考えないと、行動にうつせない。")
+        let q10  = Question(id: "10", text: "じっとおとなしくしているのが苦手である。")
+        questions.append(q1)
+        questions.append(q2)
+        questions.append(q3)
+        questions.append(q4)
+        questions.append(q5)
+        questions.append(q6)
+        questions.append(q7)
+        questions.append(q8)
+        questions.append(q9)
+        questions.append(q10)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    func setup() {
+        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName : UIFont.systemFont(ofSize: 17.0),
+                                                                        NSForegroundColorAttributeName : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)]
+        navigationItem.title = "Question"
+        //戻るボタンセット
+        let backButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backButtonItem
+    }
+
 
 }
 
@@ -48,13 +86,14 @@ extension QuestionViewController: UITableViewDelegate {
 extension QuestionViewController: UITableViewDataSource {
     // 何個のcellを生成するかを設定する関数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return questions.count
     }
     
     // 描画するcellを設定する関数
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell")
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell") as! QuestionTableViewCell
+        cell.fill(question: questions[indexPath.row])
+        return cell
     }
 }
 
