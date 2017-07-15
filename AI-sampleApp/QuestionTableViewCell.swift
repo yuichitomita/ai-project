ebe5a59551ad11f88188ea34ae77857f2c48cc31
yuichitomita <y.tomita@speedlink-td.info>
2017-07-15 22:28:17 +0900

    セグメンタルコントロール設定追加

--- AI-sampleApp/QuestionTableViewCell.swift
+++ AI-sampleApp/QuestionTableViewCell.swift
@@ -11,7 +11,20 @@ import UIKit
 class QuestionTableViewCell: UITableViewCell {
     
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    func setup(){
+        //フォント
+        let attr = NSDictionary(object: UIFont.systemFont(ofSize: 12.0), forKey: NSFontAttributeName as NSCopying)
+        segmentedControl.setTitleTextAttributes(attr as [NSObject : AnyObject], for: .normal)
+        
+        //背景
+        segmentedControl.layer.cornerRadius = 4
+        segmentedControl.clipsToBounds = true
+
+    }
+    
     func fill (question: Question) {
-        questionLabel.text = question.id + "." + question.text
+        questionLabel.text = question.id + ". " + question.text
     }
 }
