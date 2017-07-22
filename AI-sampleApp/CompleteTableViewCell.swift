71a9d0bc838fe534c86adc31549401c178b2c77d
yuichitomita <y.tomita@speedlink-td.info>
2017-07-22 18:49:12 +0900

    完了ボタン配置用セル

---
+++ AI-sampleApp/CompleteTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CompleteTableViewCell.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/07/22.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import UIKit
+
+class CompleteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var completeButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
