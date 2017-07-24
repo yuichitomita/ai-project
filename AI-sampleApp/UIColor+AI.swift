af3eb9f0a66226e8e70403a2c8d8e8a6ee6ef4e3
yuichitomita <y.tomita@speedlink-td.info>
2017-07-24 22:12:31 +0900

    UIColor Extension 作成

---
+++ AI-sampleApp/UIColor+AI.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+AI.swift
+//  AI-sampleApp
+//
+//  Created by Tomi on 2017/07/24.
+//  Copyright © 2017年 slj. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    struct Ai  {
+        init() {}
+        static let theme = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        static let fontColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        static let selectColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        static let backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+    }
+    
+}
